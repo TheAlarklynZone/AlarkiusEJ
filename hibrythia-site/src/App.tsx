a90d3d6a457b81b72c8c1e8b7cae2df61061c755
AlarkiusEJ <84819182+AlarkiusJay@users.noreply.github.com>
2026-06-17 22:43:02 +0000

    feat: add RaeyaFlutters, KydelRhunes, ZohlCelestreule routes to App.tsx

--- hibrythia-site/src/App.tsx
+++ hibrythia-site/src/App.tsx
@@ -60,6 +60,9 @@ import TheCosmicTrainFromBeyond from '@/pages/MetaWorldbuilding/TheCosmicTrainFr
 import TheOuterbounds from '@/pages/MetaWorldbuilding/TheOuterbounds';
 import CharacterProfiles from '@/pages/CharacterProfiles';
 import PhilstraRhys from '@/pages/CharacterProfiles/PhilstraRhys';
+import RaeyaFlutters from '@/pages/CharacterProfiles/RaeyaFlutters';
+import KydelRhunes from '@/pages/CharacterProfiles/KydelRhunes';
+import ZohlCelestreule from '@/pages/CharacterProfiles/ZohlCelestreule';
 import Bookshelf from '@/pages/Bookshelf';
 import Vol0HTHGOE from '@/pages/Bookshelf/Vol0HTHGOE';
 import Vol0EP0 from '@/pages/Bookshelf/Vol0HTHGOE/EP0';
@@ -191,6 +194,9 @@ export default function App() {
           <Route path="world/meta/TheSigilOfHibrythia/illusia" element={<PageTransition><IllusiaThePlaneOfDreams /></PageTransition>} />
           <Route path="characters" element={<PageTransition><CharacterProfiles /></PageTransition>} />
           <Route path="characters/PhilstraRhys" element={<PageTransition><PhilstraRhys /></PageTransition>} />
+          <Route path="characters/RaeyaFlutters" element={<PageTransition><RaeyaFlutters /></PageTransition>} />
+          <Route path="characters/KydelRhunes" element={<PageTransition><KydelRhunes /></PageTransition>} />
+          <Route path="characters/ZohlCelestreule" element={<PageTransition><ZohlCelestreule /></PageTransition>} />
           <Route path="bookshelf" element={<PageTransition><Bookshelf /></PageTransition>} />
           <Route path="bookshelf/vol0" element={<PageTransition><Vol0HTHGOE /></PageTransition>} />
           <Route path="bookshelf/vol0/ep0" element={<PageTransition><Vol0EP0 /></PageTransition>} />
