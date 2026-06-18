65d014da4f4adc78110819c030fea5186e292c47
AlarkiusEJ <84819182+AlarkiusJay@users.noreply.github.com>
2026-06-18 11:26:45 +0000

    feat: wire NahfiaAlphraey route in App.tsx

--- hibrythia-site/src/App.tsx
+++ hibrythia-site/src/App.tsx
@@ -70,6 +70,7 @@ import HunterNoxxIncogniax from '@/pages/CharacterProfiles/HunterNoxxIncogniax';
 import KingNhavah from '@/pages/CharacterProfiles/KingNhavah';
 import QueenLaeyana from '@/pages/CharacterProfiles/QueenLaeyana';
 import AloriaCloudwave from '@/pages/CharacterProfiles/AloriaCloudwave';
+import NahfiaAlphraey from '@/pages/CharacterProfiles/NahfiaAlphraey';
 import Bookshelf from '@/pages/Bookshelf';
 import Vol0HTHGOE from '@/pages/Bookshelf/Vol0HTHGOE';
 import Vol0EP0 from '@/pages/Bookshelf/Vol0HTHGOE/EP0';
@@ -211,6 +212,7 @@ export default function App() {
           <Route path="characters/KingNhavah" element={<PageTransition><KingNhavah /></PageTransition>} />
           <Route path="characters/QueenLaeyana" element={<PageTransition><QueenLaeyana /></PageTransition>} />
           <Route path="characters/AloriaCloudwave" element={<PageTransition><AloriaCloudwave /></PageTransition>} />
+          <Route path="characters/NahfiaAlphraey" element={<PageTransition><NahfiaAlphraey /></PageTransition>} />
           <Route path="bookshelf" element={<PageTransition><Bookshelf /></PageTransition>} />
           <Route path="bookshelf/vol0" element={<PageTransition><Vol0HTHGOE /></PageTransition>} />
           <Route path="bookshelf/vol0/ep0" element={<PageTransition><Vol0EP0 /></PageTransition>} />
